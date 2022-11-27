64c5085b9ffba00a0c8787c6d9314d688a92c2d7
Watson_J <skfkeh0512@naver.com>
2022-11-27 16:54:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 import time
 from PIL import Image     # 이미지 처리 라이브러리
 
+import numpy as np
+import plotly.figure_factory as ff
+
 ########### function ###########
 def count_down(ts):
     with st.empty():
@@ -92,7 +95,7 @@ with tab_Log:
    else :
         rs_text = False
         
-   st.write(f'Test_size : {ts_number}      Random_state : {rs_text}{rs_number}')
+   # st.write(f'Test_size : {ts_number}      Random_state : {rs_text}{rs_number}')
 
 #### Tab2
 with tab_RF:
@@ -104,55 +107,47 @@ with tab_XGB:
    st.header("XGBoost")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
-
-
-###############
-###############
-###############
-
-
-placeholder = st.empty()
-
-# Replace the placeholder with some text:
-placeholder.text("Hello")
-
-# Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
-
-# Replace the chart with several elements:
-with placeholder.container():
-    st.write("This is one element")
-    st.write("This is another")
-
-# Clear all those elements:
-placeholder.empty()
-
     
 
-# if choice == 'Logistic' and SearchBtn:
-#     st.session_state['chk_strline'] = 'Logistic'
+if choice == 'Logistic' and SearchBtn:
+    st.session_state['chk_strline'] = 'Logistic'
 
-# if choice == 'RandomForest' and SearchBtn:
-#     st.session_state['chk_strline'] = 'RandomForest'
+if choice == 'RandomForest' and SearchBtn:
+    st.session_state['chk_strline'] = 'RandomForest'
 
-# if choice == 'XGBoost' and SearchBtn:
-#    st.session_state['chk_strline'] = 'XGBoost'
+if choice == 'XGBoost' and SearchBtn:
+    st.session_state['chk_strline'] = 'XGBoost'
 
 
 SearchBtn = st.button('Search')
 
 
+#####
+if SearchBtn:
+    # Add histogram data
+    x1 = np.random.randn(200) - 2
+    x2 = np.random.randn(200)
+    x3 = np.random.randn(200) + 2
 
+    # Group data together
+    hist_data = [x1, x2, x3]
 
-st.write(st.session_state['chk_strline'])
+    group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+    # Create distplot with custom bin_size
+    fig = ff.create_distplot(
+            hist_data, group_labels, bin_size=[.1, .25, .5])
+
+    # Plot!
+    st.plotly_chart(fig, use_container_width=True)
+
+#####
 
 
 
 
 btn_chkbox = st.checkbox("WebCam")
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-    
 
 
 if btn_chkbox:
