381995ddc00df631f96e7ed6e707556d52f440ff
Watson_J <skfkeh0512@naver.com>
2022-11-27 16:59:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # app.py
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import time
 from PIL import Image     # 이미지 처리 라이브러리
 
-import numpy as np
-import plotly.figure_factory as ff
 
 ########### function ###########
 def count_down(ts):
@@ -123,23 +123,33 @@ SearchBtn = st.button('Search')
 
 
 #####
+# import plotly.figure_factory as ff
+
 if SearchBtn:
     # Add histogram data
+    x0 = np.random.randn(200) - 5
     x1 = np.random.randn(200) - 2
     x2 = np.random.randn(200)
     x3 = np.random.randn(200) + 2
+    x4 = np.random.randn(200) + 5
 
+
+    fig = plt.figure() 
+    plt.plot([x0, x1, x2, x3, x4]) 
+
+    st.pyplot(fig)
+    
     # Group data together
-    hist_data = [x1, x2, x3]
+#    hist_data = [x0, x1, x2, x3, x4]
 
-    group_labels = ['Group 1', 'Group 2', 'Group 3']
+#    group_labels = ['Group 1', 'Group 2', 'Group 3']
 
     # Create distplot with custom bin_size
-    fig = ff.create_distplot(
-            hist_data, group_labels, bin_size=[.1, .25, .5])
+#    fig = ff.create_distplot(
+#            hist_data, group_labels, bin_size=[.1, .25, .5])
 
     # Plot!
-    st.plotly_chart(fig, use_container_width=True)
+#    st.plotly_chart(fig, use_container_width=True)
 
 #####
 
