bf9b259dca9e273efbb2ce1b557cddfbe5e305b4
Watson_J <skfkeh0512@naver.com>
2022-11-27 16:50:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if st.session_state['chk_balloon'] == False:
 st.title('내 항공료는 왜 비싼 것인가')
 
 jpg_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00f3d481-97e5-4de9-bcf2-48c82b265793/d7uteu8-e50dde9e-b8af-4fea-ab31-b7748470dc8b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZjNkNDgxLTk3ZTUtNGRlOS1iY2YyLTQ4YzgyYjI2NTc5M1wvZDd1dGV1OC1lNTBkZGU5ZS1iOGFmLTRmZWEtYWIzMS1iNzc0ODQ3MGRjOGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X7DaOWcJkNe2H8jjTNtybdRCV9p5u4H_yFaOk7kMbFg"
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 st.image(jpg_url, caption="Why So Serious??!")
 
 st.write(f"사용한 데이터 URL : {url}")
@@ -74,6 +74,7 @@ st.write("분석 알고리즘을 골라주세요")
 
 tab_Log, tab_RF, tab_XGB = st.tabs(["Logistic", "RandomForest", "XGBoost"])
 
+#### Tab1
 with tab_Log:
    st.header("Logistic")
    st.image("https://github.com/skfkeh/newthing/blob/main/img/Patrick.jpeg?raw=true", width=200)
@@ -90,12 +91,15 @@ with tab_Log:
                               step=50, format="%d")
    else :
         rs_text = False
-#   st.write(f'Test_size : {ts_number}      Random_state : {rs_text}{rs_number}')
+        
+   st.write(f'Test_size : {ts_number}      Random_state : {rs_text}{rs_number}')
 
+#### Tab2
 with tab_RF:
    st.header("RandomForest")
    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
 
+#### Tab3
 with tab_XGB:
    st.header("XGBoost")
    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
