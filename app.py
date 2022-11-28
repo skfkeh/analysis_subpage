17fec4a730d601fe7781c8628a13e7bbe587ebb6
Watson_J <skfkeh0512@naver.com>
2022-11-28 17:03:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -375,8 +375,11 @@ elif options == '03. 시각화(plotly)':
             st.write(f'Train_set : {model.score(X_train, y_train)}')
             st.write(f'Test_set : {model.score(X_test, y_test)}')
             
-            st.write(f'train_pred " {train_pred}')
-            st.write(f'test_pred " {test_pred}')
+            st.write('train_pred')            
+            st.dataframe(train_pred)
+            st.write('')
+            st.write('test_pred')
+            st.dataframe(test_pred)
 
         # 훈련 모델 시각화
         st.subheader('모델 훈련이 잘 되었는지 시각화')
