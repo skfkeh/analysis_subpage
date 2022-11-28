a4806bd404efb7c0364d221c43856461507ceeb3
Watson_J <skfkeh0512@naver.com>
2022-11-28 17:33:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -376,7 +376,7 @@ elif options == '03. 시각화(plotly)':
             df_pred = pd.DataFrame(
                 [['Set', model.score(X_train, y_train), model.score(X_test, y_test)],
                  ['RMSE', mean_squared_error(y_train, train_pred, squared=False), mean_squared_error(y_test, test_pred)]],
-                columns=(['', 'Test', 'Train'])
+                columns=(['', 'Test', 'Train'], inplace=True)
             )
 
             st.table(df_pred)
