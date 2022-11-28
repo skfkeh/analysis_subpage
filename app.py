94df73a61015736cff3f48458f5cf0a91c140339
Watson_J <skfkeh0512@naver.com>
2022-11-28 11:09:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from PIL import Image     # 이미지 처리 라이브러리
 
 
 ########### function ###########
+## 카운트 다운
 def count_down(ts):
     with st.empty():
         input_time = 1*3
@@ -21,6 +22,72 @@ def count_down(ts):
             input_time -= 1
         st.empty()
 
+## 데이터 전처리
+def preprocessing(df):
+    ### 1. Route Drop 처리
+    df.drop('Route', axis=1, inplace=True)
+
+    ### 2. Duration 컬럼을 '시간'과 '분' 단위로 분할 후 Duration 컬럼 drop
+    df['Dep_Time'] = pd.to_datetime(df['Dep_Time'], format= '%H:%M').dt.time
+    df['Duration_hour'] = df.Duration.str.extract('(\d+)h')
+    df['Duration_min'] = df.Duration.str.extract('(\d+)m').fillna(0)
+    df.drop('Duration', axis=1, inplace=True)
+    df.drop(index=6474,inplace=True)
+
+    df.Duration_hour = df.Duration_hour.astype('int64')
+    df.Duration_min = df.Duration_min.astype('int64')
+    df.Duration_hour = df.Duration_hour*60
+    df['Duration_total'] = df.Duration_hour+df.Duration_min
+    df.drop(columns=['Duration_hour','Duration_min','Arrival_Time'],inplace=True)
+
+    ### 3. Airline 전처리
+    air_count = df.Airline.value_counts().index
+    airlist = [l for l in air_count if list(df.Airline).count(l) < 200]
+    df.Airline = df.Airline.replace(airlist, 'Others')
+
+    for t in range(len(air_count)):
+        df.loc[df.Airline == air_count[t], 'Air_col'] = t
+    df.drop(columns=['Airline'],inplace=True)
+
+    ### 4. Additional_Info 전처리
+    add_count = df.Additional_Info.value_counts().index
+    additional_thing = [l for l in add_count if list(df.Additional_Info).count(l) < 20]
+    df.Additional_Info = df.Additional_Info.replace(additional_thing, 'Others')
+
+    add_count = df.Additional_Info.value_counts().index
+    for t in range(len(add_count)):
+        df.loc[df.Additional_Info == add_count[t], 'Add_col'] = t
+
+    ### 5. Total_Stops 전처리
+    df.loc[df.Total_Stops.isna(),'Total_Stops'] = '1 stop'
+
+    def handle_stops(x):
+        if x == 'non-stop': return 0
+        return int(x.split()[0])
+
+    df.Total_Stops = df.Total_Stops.apply(handle_stops)
+
+    ### 6. Date_of_Journey 전처리
+    df['Date_of_journey_DT'] = pd.to_datetime(df['Date_of_Journey'])
+    df['weekday'] = pd.to_datetime(df['Date_of_journey_DT']).dt.weekday
+    df['weekday_name'] = pd.to_datetime(df['Date_of_journey_DT']).dt.day_name()
+
+    ### 7. Dep_Time 데이터 전처리
+    df.Dep_Time = df.Dep_Time.astype(str)
+    df['Dep_hour'] = df.Dep_Time.str.extract('([0-9]+)\:')
+    df.drop(columns=['Dep_Time'],inplace=True)
+
+    ### 8. 불필요 컬럼 drop
+    df.drop(columns=['Date_of_Journey',
+                     'Source','Destination',
+                     'Date_of_journey_DT',
+                     'Additional_Info',
+                     'weekday'],inplace=True)
+
+    ### 9.범주형 변수 처리
+    df = pd.get_dummies(df, columns=['weekday_name','Add_col','Air_col'],drop_first=True)
+    
+    return df
 ########### function ###########
         
     
@@ -78,12 +145,23 @@ if options == '01. Home':
     st.write(f"사용한 데이터 URL : {url}")
 
 elif options == '02. 데이터 전처리 과정':
+    df = read_csv(url)
+    
+    st.write("df.head()")
+    st.dataframe(df.head())
+    
+    st.write("df.head()")
+    st.dataframe(df.info())
+    
+    pre_data = preprocessing(df)
+    
+elif options == '03. 시각화(plotly)':
     st.write("분석 알고리즘을 골라주세요")
 
-    tab_Log, tab_RF, tab_XGB = st.tabs(["Logistic", "RandomForest", "XGBoost"])
+    tab_De, tab_RF, tab_XGB = st.tabs(["DecisionTree", "RandomForest", "XGBoost"])
 
     #### Tab1
-    with tab_Log:
+    with tab_De:
        col1, col2 = st.columns(2)
 
        st.header("Logistic")
@@ -109,7 +187,8 @@ elif options == '02. 데이터 전처리 과정':
        st.header("XGBoost")
        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
-elif options == '03. 시각화(plotly)':
+        
+        
     SearchBtn = st.button('Search')
     
     if SearchBtn:
