5754a9405497339aab80293c59aa8f8770fbdfc6
Watson_J <skfkeh0512@naver.com>
2022-11-28 11:45:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -155,20 +155,20 @@ elif options == '02. 데이터 전처리 과정':
     
     st.write("2. Route Drop 처리")
     code_Route = '''df.drop('Route', axis=1, inplace=True)'''
-    st.code(code_Route, langauge='python')
+    st.code(code_Route, language='python')
     
     st.write("3. Duration 컬럼을 '시간'과 '분' 단위로 분할 후 Duration 컬럼 drop")
     code_Dep = '''df['Dep_Time'] = pd.to_datetime(df['Dep_Time'], format= '%H:%M').dt.time
     df['Duration_hour'] = df.Duration.str.extract('(\d+)h')
     df['Duration_min'] = df.Duration.str.extract('(\d+)m').fillna(0)
     '''
-    st.code(code_Dep, langauge='python')
+    st.code(code_Dep, language='python')
     
     code_airlist = '''airlist = [l for l in air_count if list(df.Airline).count(l) < 200]
     df.Airline = df.Airline.replace(airlist, 'Others')
     '''
     
-    st.code(code_airlist, langauge='python')
+    st.code(code_airlist, language='python')
     
     pre_data = preprocessing(df)
     
