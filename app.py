3b75751563f527f346348934c65039a11d614c78
Watson_J <skfkeh0512@naver.com>
2022-11-28 09:23:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -120,17 +120,17 @@ with tab_XGB:
 
     
 
-if choice == 'Logistic' and SearchBtn:
-    st.session_state['chk_strline'] = 'Logistic'
+# if choice == 'Logistic' and SearchBtn:
+#     st.session_state['chk_strline'] = 'Logistic'
 
-if choice == 'RandomForest' and SearchBtn:
-    st.session_state['chk_strline'] = 'RandomForest'
+# if choice == 'RandomForest' and SearchBtn:
+#     st.session_state['chk_strline'] = 'RandomForest'
 
-if choice == 'XGBoost' and SearchBtn:
-    st.session_state['chk_strline'] = 'XGBoost'
+# if choice == 'XGBoost' and SearchBtn:
+#     st.session_state['chk_strline'] = 'XGBoost'
 
 
-SearchBtn = st.button('Search')
+# SearchBtn = st.button('Search')
 
 
 #####
@@ -154,30 +154,3 @@ if SearchBtn:
 
     # Plot!
     st.plotly_chart(fig, use_container_width=True)
-
-#####
-
-
-
-
-# btn_chkbox = st.checkbox("WebCam")
-# st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-
-
-# if btn_chkbox:
-#     img_file_buffer = st.camera_input("Take a picture")
-
-#     if img_file_buffer is not None:
-#         # To read image file buffer as a 3D uint8 tensor with PyTorch:
-#         bytes_data = img_file_buffer.getvalue()
-#         torch_img = torch.ops.image.decode_image(
-#             torch.from_numpy(np.frombuffer(bytes_data, np.uint8)), 3
-#         )
-
-#         # Check the type of torch_img:
-#         # Should output: <class 'torch.Tensor'>
-#         st.write(type(torch_img))
-
-#         # Check the shape of torch_img:
-#         # Should output shape: torch.Size([channels, height, width])
-#         st.write(torch_img.shape)
