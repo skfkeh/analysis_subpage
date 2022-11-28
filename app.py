a13493ee124b18519ac6634a3382a0d48abe5f8d
Watson_J <skfkeh0512@naver.com>
2022-11-28 16:07:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ options = st.sidebar.radio('Why is my airfare expensive?!', options=['01. Home',
 
 if options == '01. Home':
     st.title('내 항공료는 왜 비싼 것인가')
-    st.header('다음 항목은 사이드 메뉴를 확인해 주세요.')
+    st.write('다음 항목은 사이드 메뉴를 확인해 주세요.')
 
     jpg_url = "https://github.com/skfkeh/MachineLearning/blob/main/img/why.png?raw=true"
     # st.set_page_config(layout="wide")
