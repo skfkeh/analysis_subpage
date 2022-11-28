09d22a62bca6600df7721018b070807c0de3e223
Watson_J <skfkeh0512@naver.com>
2022-11-28 19:45:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -409,15 +409,15 @@ elif options == '03. 알고리즘 적용':
         st.write(f'train 결정계수 : {train_relation_square_rf}')
         st.write(f'test 결정계수 : {test_relation_square_rf}')
 
-#         st.subheader('시각화 부분')
-#         checkBox_rf = st.checkbox('plotly 활성화')
+        st.subheader('시각화 부분')
+        checkBox_rf = st.checkbox('plotly 활성화')
 
-#         if checkBox_rf:
-#             fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
-#             fig.add_trace(go.Scatter(x=y_train,y=y_test, mode='markers',name='Actual'))
-#             fig.add_trace(go.Scatter(x=y_test,y=test_pred,mode='markers',name='Predict')) # mode='lines+markers'
-#             fig.update_layout(title='<b>actual과 predict 비교')
-#             st.plotly_chart(fig)
+        if checkBox_rf:
+            fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+            fig.add_trace(go.Scatter(x=y_train,y=y_test, mode='markers',name='Actual'))
+            fig.add_trace(go.Scatter(x=y_test,y=test_pred,mode='markers',name='Predict')) # mode='lines+markers'
+            fig.update_layout(title='<b>actual과 predict 비교')
+            st.plotly_chart(fig)
 
     #### Tab3
     with tab_XGB:
