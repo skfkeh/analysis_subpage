028686e2a7d1fa39f0e7db5dc2bff2270d51887f
Watson_J <skfkeh0512@naver.com>
2022-11-28 09:39:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -67,17 +67,18 @@ options = st.sidebar.radio('Why is my airfare expensive?!', options=['01. Home',
 # if uploaded_file:
 #    df = pd.read_excel(url)
 
-st.title('내 항공료는 왜 비싼 것인가')
+if options == '01. Home':
+    st.title('내 항공료는 왜 비싼 것인가')
 
-st.header('다음 항목은 사이드 메뉴를 확인해 주세요.')
+    st.subtitle('다음 항목은 사이드 메뉴를 확인해 주세요.')
 
-jpg_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00f3d481-97e5-4de9-bcf2-48c82b265793/d7uteu8-e50dde9e-b8af-4fea-ab31-b7748470dc8b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZjNkNDgxLTk3ZTUtNGRlOS1iY2YyLTQ4YzgyYjI2NTc5M1wvZDd1dGV1OC1lNTBkZGU5ZS1iOGFmLTRmZWEtYWIzMS1iNzc0ODQ3MGRjOGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X7DaOWcJkNe2H8jjTNtybdRCV9p5u4H_yFaOk7kMbFg"
-# st.set_page_config(layout="wide")
-st.image(jpg_url, caption="Why So Serious??!")
+    jpg_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00f3d481-97e5-4de9-bcf2-48c82b265793/d7uteu8-e50dde9e-b8af-4fea-ab31-b7748470dc8b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZjNkNDgxLTk3ZTUtNGRlOS1iY2YyLTQ4YzgyYjI2NTc5M1wvZDd1dGV1OC1lNTBkZGU5ZS1iOGFmLTRmZWEtYWIzMS1iNzc0ODQ3MGRjOGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X7DaOWcJkNe2H8jjTNtybdRCV9p5u4H_yFaOk7kMbFg"
+    # st.set_page_config(layout="wide")
+    st.image(jpg_url, caption="Why So Serious??!")
 
-st.write(f"사용한 데이터 URL : {url}")
+    st.write(f"사용한 데이터 URL : {url}")
 
-if options == '02. 데이터 전처리 과정':
+elif options == '02. 데이터 전처리 과정':
     st.write("분석 알고리즘을 골라주세요")
 
     tab_Log, tab_RF, tab_XGB = st.tabs(["Logistic", "RandomForest", "XGBoost"])
