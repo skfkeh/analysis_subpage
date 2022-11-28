c3bec166375d208c5014648c3dbc4bac59924269
Watson_J <skfkeh0512@naver.com>
2022-11-28 09:35:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,13 +62,15 @@ if st.session_state['chk_balloon'] == False:
 ################################
 
 
-options = st.sidebar.radio('Pages', options=['Home','Data preprocessing','Plot'])
+options = st.sidebar.radio('Machine Learning\n Why is my airfare expensive?!', options=['01. Home','02. 데이터 전처리 과정','03. 시각화(plotly)'])
 
 # if uploaded_file:
 #    df = pd.read_excel(url)
 
 if options == 'Home':
     st.title('내 항공료는 왜 비싼 것인가')
+    
+    st.header('다음 항목은 사이드 메뉴를 확인해 주세요.')
 
     jpg_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/00f3d481-97e5-4de9-bcf2-48c82b265793/d7uteu8-e50dde9e-b8af-4fea-ab31-b7748470dc8b.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAwZjNkNDgxLTk3ZTUtNGRlOS1iY2YyLTQ4YzgyYjI2NTc5M1wvZDd1dGV1OC1lNTBkZGU5ZS1iOGFmLTRmZWEtYWIzMS1iNzc0ODQ3MGRjOGIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.X7DaOWcJkNe2H8jjTNtybdRCV9p5u4H_yFaOk7kMbFg"
     # st.set_page_config(layout="wide")
